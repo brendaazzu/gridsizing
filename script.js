2c3419daa70f8b6477317f43d551b02352c9a132
Brenda Azzu <100886386+brendaazzu@users.noreply.github.com>
2024-03-05 18:26:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+
+    function showPreview1() {
+        var preview = document.getElementById('preview1');
+        preview.src = './fixed-size.html';
+        preview.style.display = 'block';
+    }
+
+    function hidePreview1() {
+        var preview = document.getElementById('preview1');
+        preview.style.display = 'none';
+    }
+
+
+    function showPreview2() {
+        var preview = document.getElementById('preview2');
+        preview.src = './auto-size.html';
+        preview.style.display = 'block';
+    }
+
+    function hidePreview2() {
+        var preview = document.getElementById('preview2');
+        preview.style.display = 'none';
+    }
+
+
+    function showPreview3() {
+        var preview = document.getElementById('preview3');
+        preview.src = './fractional-size.html';
+        preview.style.display = 'block';
+    }
+
+    function hidePreview3() {
+        var preview = document.getElementById('preview3');
+        preview.style.display = 'none';
+    }
+
+
+    function showPreview4() {
+        var preview = document.getElementById('preview4');
+        preview.src = './minmax-size.html';
+        preview.style.display = 'block';
+    }
+
+    function hidePreview4() {
+        var preview = document.getElementById('preview4');
+        preview.style.display = 'none';
+    }
+
+
+   function showPreview5() {
+        var preview = document.getElementById('preview5');
+        preview.src = './repeat.html';
+        preview.style.display = 'block';
+    }
+
+    function hidePreview5() {
+        var preview = document.getElementById('preview5');
+        preview.style.display = 'none';
+    }                     
+
+    function showPreview6() {
+        var preview = document.getElementById('preview6');
+        preview.src = './test.html';
+        preview.style.display = 'block';
+    }
+
+    function hidePreview6() {
+        var preview = document.getElementById('preview6');
+        preview.style.display = 'none';
+    }
